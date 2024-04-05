ff8ae0e2d7d35f54ae917700fe6ed9b5709700d7
ShikiBo(房士琦) <shiki.bo.f704@dirbato.co.jp>
2024-04-05 11:50:34 +0900

    「Gitの練習です」

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");        // TODO 自動生成されたメソッド・スタブ
-
+        System.out.println("こんにちは、世界！こんにちは、世界！");
+        System.out.println("こんにちは、世界！");
     }
 
 }
